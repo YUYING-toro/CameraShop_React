caa385a2c566992e8879483a498741ecd1b7e607
Sandra <000@gmail.com>
2021-09-23 15:46:30 -0400

    set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <p className="mt-s">HELLP</p>
+    
+  );
+}
+
+export default App;
