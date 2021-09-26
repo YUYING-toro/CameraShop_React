134c038b309da6618026a80e16008eb2e877c1c8
Sandra <000@gmail.com>
2021-09-26 14:22:59 -0400

    import particles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { React } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Particles from 'react-particles-js';
 import Test from "./Components/test";
 import { Header } from "./Components/Header";
 // 幾何轉
@@ -11,7 +12,26 @@ import { Header } from "./Components/Header";
 function App() {
   return (
   <>
-
+    <Particles
+      params={{
+        particles: {
+          number: {
+            value: 200,
+            density: {
+              enable: true,
+              value_area: 1000,
+            }
+          },
+          shape:{
+            type:"star",
+            stroke:{
+              width: 4,
+              color: "orange"
+            }
+          }
+        },
+      }}
+    />
     <Test />
     <Header />
   </>
